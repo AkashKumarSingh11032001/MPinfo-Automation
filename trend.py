bd8cf6c82893b8bb464637661d2b2ae33a7f4aa8
Akash <akashkumarsingh11032001@gmail.com>
2022-10-23 08:19:22 +0530

    upadted for PF and VF

--- trend.py
+++ trend.py
@@ -13,102 +13,81 @@ from supportFunction import hexToBytes
 from supportFunction import pathConvertion
 from VF1 import VF1
 
-files = ["mpInfo_bin_log\\MPInfoBuf.bin"]
+
+def PF0(parameter):
+    
+    path,level,serialNo,driveCap,firmwareRev,modelNo,HexLine = parameter
+    
+    files = [path]
+
+    filename = files[0]
+    with open(filename, 'rb') as f:
+        content = f.read()
 
 
-# level = input("Enter the Physical Function name. : \n")
-# serialNo = input("Enter the Serial Number. : \n")
-# driveCap = input("Enter the Drive Capacity. : \n")
-# firmwareRev = input("Enter the Firmware Revision. : \n")
-# modelNo = input("Enter the Model Number. : \n")
+    Hexify_String = binascii.hexlify(content)
+    print(len(Hexify_String))
 
-# # user input:
-level = "PF1"
-serialNo = "0000001"
-driveCap = "256G"
-firmwareRev = "WS20"
-modelNo = "SUBNQN1"
+    HexList = [Hexify_String[i:i+32] for i in range(0, len(Hexify_String), 32)]
 
 
-filename = files[0]
-with open(filename, 'rb') as f:
-    content = f.read()
+    HexLineList = HexLine
+    DecimalLineList = [int(str(i), base=16) for i in HexLineList]  # [288, 290, 291, 295, 384]
 
 
-Hexify_String = binascii.hexlify(content)
-print(len(Hexify_String))
+    serial_x = serialUpdate(str=serialNo, level=level)
+    drivecap_x = updateDriveCap(cap=driveCap, level=level)
+    firmware_x = updateFirmware(firmware=firmwareRev, date=todaysDate())
+    model_x = updateModel(model=modelNo, level=level)
 
-HexList = [Hexify_String[i:i+32] for i in range(0, len(Hexify_String), 32)]
+    # convert each updated value to hex
+    serial_x_hex = stringToHex(serial_x)
+    drivecap_x_hex = stringToHex(drivecap_x)
+    firmware_x_hex = stringToHex(firmware_x)
+    model_x_hex = stringToHex(model_x)
 
+    # handling extra bits
+    extra_byte = "\x00"*32
+    extra_x_hex = stringToHex(extra_byte)
 
-HexLineList = [120, 122, 123, 127, 180]
-DecimalLineList = [int(str(i), base=16) for i in HexLineList]  # [288, 290, 291, 295, 384]
-
-
-serial_x = serialUpdate(str=serialNo, level=level)
-drivecap_x = updateDriveCap(cap=driveCap, level=level)
-firmware_x = updateFirmware(firmware=firmwareRev, date=todaysDate())
-model_x = updateModel(model=modelNo, level=level)
-
-# convert each updated value to hex
-serial_x_hex = stringToHex(serial_x)
-drivecap_x_hex = stringToHex(drivecap_x)
-firmware_x_hex = stringToHex(firmware_x)
-model_x_hex = stringToHex(model_x)
-
-# handling extra bits
-extra_byte = "\x00"*32
-extra_x_hex = stringToHex(extra_byte)
-
-if len(drivecap_x_hex) > 32:
-    left = len(drivecap_x_hex) - 32
-    r = drivecap_x_hex
-    drivecap_x_hex = r[:32]
-    extra_x_hex = r[32:] + extra_x_hex[left:]
+    if len(drivecap_x_hex) > 32:
+        left = len(drivecap_x_hex) - 32
+        r = drivecap_x_hex
+        drivecap_x_hex = r[:32]
+        extra_x_hex = r[32:] + extra_x_hex[left:]
 
 
-temp = [serial_x_hex, drivecap_x_hex, extra_x_hex, firmware_x_hex, model_x_hex]
-final = []  # help to update directly to hexLIst.
-for i in range(0, len(temp)):
-    ln = len(temp[i])
-    print(ln)
-    if ln < 32:
-        z = 32 - ln
-        final.append(temp[i] + "0"*z)
-    else:
-        final.append(temp[i])
+    temp = [serial_x_hex, drivecap_x_hex, extra_x_hex, firmware_x_hex, model_x_hex]
+    final = []  # help to update directly to hexLIst.
+    for i in range(0, len(temp)):
+        ln = len(temp[i])
+        print(ln)
+        if ln < 32:
+            z = 32 - ln
+            final.append(temp[i] + "0"*z)
+        else:
+            final.append(temp[i])
 
-print(final)  # hex not converted to bytes...
-byteFinal = [hexToBytes(i) for i in final]
-print(byteFinal)  # hex converted to bytes...
-
-
-# updating HexList with the help of byteFinal...
-j = 0
-for i in DecimalLineList:
-    HexList[i] = byteFinal[j]
-    j = j + 1
-
-# converting HexList to bytes String...
-updatedString = b''.join(HexList)
-
-# UnHexlify the bytesString to hexString...
-unHexify_String = binascii.unhexlify(updatedString)
+    print(final)  # hex not converted to bytes...
+    byteFinal = [hexToBytes(i) for i in final]
+    print(byteFinal)  # hex converted to bytes...
 
 
-# creating new MPinfo.bin file with user inputed updated information
-with open('MPinfo.bin', 'wb') as f:
-    f.write(unHexify_String)
+    # updating HexList with the help of byteFinal...
+    j = 0
+    for i in DecimalLineList:
+        HexList[i] = byteFinal[j]
+        j = j + 1
+
+    # converting HexList to bytes String...
+    updatedString = b''.join(HexList)
+
+    # UnHexlify the bytesString to hexString...
+    unHexify_String = binascii.unhexlify(updatedString)
 
 
-# getting current Path
-current_path = os.getcwd()+"\\MPinfo.bin"
-current_path = pathConvertion(current_path)
-
-VF1([current_path, "VF1", serialNo, driveCap, firmwareRev, modelNo, [300, 302, 303, 304, 306]])
+    # creating new MPinfo.bin file with user inputed updated information
+    with open('MPinfo.bin', 'wb') as f:
+        f.write(unHexify_String)
 
 
-# user input PATH :-
-# >>> from path import path
-# >>> path('mydir/myfile.txt').abspath()
-# 'C:\\example\\cwd\\mydir\\myfile.txt'
