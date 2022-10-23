1f95bc863d027201463cb7b37ede6d0c49fe72db
Akash <akashkumarsingh11032001@gmail.com>
2022-10-23 08:49:10 +0530

    pre-fin update

--- main.py
+++ main.py
@@ -15,16 +15,13 @@ from f1_PF0 import PF0
 from f2_VF1 import VF1
 
 
-file_path = "mpInfo_bin_log\\MPInfoBuf.bin"
+mp_file_path = "mpInfo_bin_log\\MPInfoBuf.bin"
 
-# level = input("Enter the Physical Function name. : \n")
 # serialNo = input("Enter the Serial Number. : \n")
 # driveCap = input("Enter the Drive Capacity. : \n")
 # firmwareRev = input("Enter the Firmware Revision. : \n")
 # modelNo = input("Enter the Model Number. : \n")
 
-# # user input:
-level = "PF1"
 serialNo = "0000001"
 driveCap = "256G"
 firmwareRev = "WS20"
@@ -35,30 +32,30 @@ modelNo = "SUBNQN1"
 current_path = os.getcwd()+"\\MPinfo.bin"
 current_path = pathConvertion(current_path)
 
-PF0([file_path, level, serialNo, driveCap, firmwareRev, modelNo, [120, 122, 123, 127, 180]])
+PF0([mp_file_path, "PF1", serialNo, driveCap, firmwareRev, modelNo, [120, 122, 123, 127, 180]])
+print("Physical Function Updated!!!")
+sleep(3)
 VF1([current_path, "VF1", serialNo, driveCap, firmwareRev, modelNo, [300, 302, 303, 304, 306]])
-VF1([current_path, "VF2", serialNo, driveCap, firmwareRev, modelNo, [320, 322, 323, 324, 326]])
-VF1([current_path, "VF3", serialNo, driveCap, firmwareRev, modelNo, [340, 342, 343, 344, 346]])
-VF1([current_path, "VF4", serialNo, driveCap, firmwareRev, modelNo, [360, 362, 363, 364, 366]])
-VF1([current_path, "VF5", serialNo, driveCap, firmwareRev, modelNo, [380, 382, 383, 384, 386]])
-VF1([current_path, "VF6", serialNo, driveCap, firmwareRev, modelNo, ["3a0", "3a2", "3a3", "3a4", "3a6"]])
-VF1([current_path, "VF7", serialNo, driveCap, firmwareRev, modelNo, ["3c0", "3c2", "3c3", "3c4", "3c6"]])
-VF1([current_path, "VF8", serialNo, driveCap, firmwareRev, modelNo, ["3e0", "3e2", "3e3", "3e4", "3e6"]])
-
-
-
-
-# 3200  S0000001VF1
-# 3220  WDSSDATPOCM256GVF1
-# 3240  WS211310(day/month) --> (edit : from 9th bit which will be default CFW)
-# 3260  SUBNQN1VF1
-
-# 3400 / 3420 / 3440 / 3460
-# 3600 / 
-# 3800 / 
-# 3a00 / 
-# 3c00 / 
-# 3e00 / 
-
-
-
+print("Virtual Function [ VF - 1 ] Updated!!!")
+sleep(3)
+VF2([current_path, "VF2", serialNo, driveCap, firmwareRev, modelNo, [320, 322, 323, 324, 326]])
+print("Virtual Function [ VF - 2 ] Updated!!!")
+sleep(3)
+VF3([current_path, "VF3", serialNo, driveCap, firmwareRev, modelNo, [340, 342, 343, 344, 346]])
+print("Virtual Function [ VF - 3 ] Updated!!!")
+sleep(3)
+VF4([current_path, "VF4", serialNo, driveCap, firmwareRev, modelNo, [360, 362, 363, 364, 366]])
+print("Virtual Function [ VF - 4 ] Updated!!!")
+sleep(3)
+VF5([current_path, "VF5", serialNo, driveCap, firmwareRev, modelNo, [380, 382, 383, 384, 386]])
+print("Virtual Function [ VF - 5 ] Updated!!!")
+sleep(3)
+VF6([current_path, "VF6", serialNo, driveCap, firmwareRev, modelNo, ["3a0", "3a2", "3a3", "3a4", "3a6"]])
+print("Virtual Function [ VF - 6 ] Updated!!!")
+sleep(3)
+VF7([current_path, "VF7", serialNo, driveCap, firmwareRev, modelNo, ["3c0", "3c2", "3c3", "3c4", "3c6"]])
+print("Virtual Function [ VF - 7 ] Updated!!!")
+sleep(3)
+VF8([current_path, "VF8", serialNo, driveCap, firmwareRev, modelNo, ["3e0", "3e2", "3e3", "3e4", "3e6"]])
+print("Virtual Function [ VF - 8 ] Updated!!!")
+sleep(3)
