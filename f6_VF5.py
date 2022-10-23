60ae9d4180b976814838d55d715d1a965574a66d
Akash <akashkumarsingh11032001@gmail.com>
2022-10-23 08:53:23 +0530

    importing VF5

--- f6_VF5.py
+++ f6_VF5.py
@@ -11,7 +11,7 @@ from supportFunction import updateFirmware
 from supportFunction import updateModel
 from supportFunction import hexToBytes
 
-def VF1(parameter):
+def VF5(parameter):
     
     current_path,level,serialNo,driveCap,firmwareRev,modelNo,HexLine = parameter
 
--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from f2_VF1 import VF1
 from f3_VF2 import VF2
 from f4_VF3 import VF3
 from f5_VF4 import VF4
+from f6_VF5 import VF5
 
 
 mp_file_path = "mpInfo_bin_log\\MPInfoBuf.bin"
